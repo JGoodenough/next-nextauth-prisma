0aa2f3f4d965b3e26bb5874d949defae7fe9cd9b
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-12-07 17:30:26 -0500

    add home id ssg page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+const prisma = new PrismaClient();
 
 export default function Home({ homes = [] }) {
   return (
@@ -19,7 +20,6 @@ export default function Home({ homes = [] }) {
 }
 
 export async function getServerSideProps() {
-  const prisma = new PrismaClient();
   const homes = await prisma.home.findMany();
   return {
     props: {
