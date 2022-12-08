3cee51a194a67f326207ced4a69ecf3481e616ee
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-12-08 14:00:00 -0500

    implement client nextauth first steps

--- components/Layout.js
+++ components/Layout.js
@@ -15,12 +15,13 @@ import {
   UserIcon,
 } from '@heroicons/react/outline';
 import { ChevronDownIcon } from '@heroicons/react/solid';
+import { useSession, signOut } from 'next-auth/react';
 
 const menuItems = [
   {
     label: 'List a new home',
     icon: PlusIcon,
-    href: '/list',
+    href: '/homes/new',
   },
   {
     label: 'My homes',
@@ -35,17 +36,17 @@ const menuItems = [
   {
     label: 'Logout',
     icon: LogoutIcon,
-    onClick: () => null,
+    onClick: signOut,
   },
 ];
 
 const Layout = ({ children = null }) => {
   const router = useRouter();
-
+  const { data: session, status } = useSession();
   const [showModal, setShowModal] = useState(false);
 
-  const user = null;
-  const isLoadingUser = false;
+  const user = session?.user;
+  const isLoadingUser = status === 'loading';
 
   const openModal = () => setShowModal(true);
   const closeModal = () => setShowModal(false);
