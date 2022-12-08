b2dd4359b55b311464d518df80193217a89c1eaf
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-12-08 12:08:42 -0500

    set up prisma adapter update prisma schema for user, session, account verification token for authentication

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import nodemailer from 'nodemailer';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+// Instantiate Prisma Client
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -17,4 +22,5 @@ export default NextAuth({
       maxAge: 10 * 60, // Magic links are valid for 10 min only
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 });
