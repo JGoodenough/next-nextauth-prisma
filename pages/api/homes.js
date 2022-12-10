540064cd8b9392fc55715a35d6309791698c8aaf
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-12-09 21:14:18 -0500

    allow creation of new homes from user

--- pages/api/homes.js
+++ pages/api/homes.js
@@ -1,15 +1,36 @@
 import { prisma } from '@/services/prisma';
+import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized.' });
+  }
+
   switch (req.method) {
     case 'POST':
       try {
         const { image, title, description, price, guests, beds, baths } =
           req.body;
 
-        const home = await prisma.home.create({
-          data: { image, title, description, price, guests, beds, baths },
+        // Retrieve the current authenticated user
+        const user = await prisma.user.findUnique({
+          where: { email: session.user.email },
         });
+
+        const home = await prisma.home.create({
+          data: {
+            image,
+            title,
+            description,
+            price,
+            guests,
+            beds,
+            baths,
+            userId: user.id,
+          },
+        });
+
         res.status(200).json(home);
       } catch (e) {
         res.status(500).json({ message: 'Something went wrong' });
