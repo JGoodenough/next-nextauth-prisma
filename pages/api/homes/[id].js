303d3d2a3d567093d2fc52f4f719ee579ecfced3
Jesse Goodenough <jesse.goodenough@gmail.com>
2022-12-13 15:01:54 -0500

    add delete home endpoint

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -1,4 +1,6 @@
 import { withServerAuth } from '@/lib/server.with-auth';
+import { prisma } from '@/services/prisma';
+import { supabase } from '@/services/supabase';
 
 export default withServerAuth(async function handler(req, res) {
   const { id } = req.query;
@@ -13,6 +15,23 @@ export default withServerAuth(async function handler(req, res) {
         const home = await prisma.home.update({
           where: { id },
           data: req.body,
+        });
+        // Remove image from Supabase storage
+        if (home.image) {
+          const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+          await supabase.storage
+            .from(process.env.SUPABASE_BUCKET)
+            .remove([path]);
+        }
+        res.status(200).json(home);
+      } catch (e) {
+        res.status(500).json({ message: 'Something went wrong' });
+      }
+      break;
+    case 'DELETE':
+      try {
+        const home = await prisma.home.delete({
+          where: { id },
         });
         res.status(200).json(home);
       } catch (e) {
